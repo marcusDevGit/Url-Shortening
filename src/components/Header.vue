07db7034cc42e28c4cfdb8ffa319d66a80f7f6fc
Marcus Phellypp <marcus_psc@hotmail.com>
2024-03-23 16:26:38 -0300

    start project

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="nav_container">
+        <a href="/" class="img_logo"><img src="/src/assets/images/logo.svg" alt="shortly image logo"></a>
+        <section class="menu">
+            <MenuLinks/>
+        </section>
+
+    </nav>
+</template>
+
+<script setup>
+import MenuLinks from './NavBar/MenuLinks.vue';
+</script>
+
+<style scoped>
+/* Your component's CSS styles go here */
+nav {
+    display: flex;
+    padding: 30px;
+    align-items: center;
+    justify-content: space-between;
+    position: relative;
+}
+
+.menu {
+    width: 100%;
+    justify-content: space-between;
+}
+
+.img_logo {
+    width: 100px;
+    position: relative;
+}
+</style>
---
+++ src/components/NavBar/MenuLinks.vue
@@ -0,0 +1,22 @@
+<template>
+    <ul class="menu_links">
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Resources</a></li>
+    </ul>
+</template>
+<style scoped>
+.menu_links{
+    display: flex;
+    justify-content: flex-start;
+    gap: 3rem;
+}
+a{
+    color: var(--grayishviolet);
+    font-weight: 700;
+}
+a:hover{
+    color: var(--darkviolet);
+    cursor: pointer;
+}
+</style>
